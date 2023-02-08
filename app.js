33de6f854fd6864e6ca31f1b7dd48dcd103298d9
teffidev <tefyberal09@gmail.com>
2023-02-08 14:36:36 -0500

    Add dependencie

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/api", router);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + './views');
 
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
